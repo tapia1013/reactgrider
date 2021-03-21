3674c09eee46c1c38687b186e266e4a868aa8ce2
tapia1013 <manbearpig1013@gmail.com>
2021-03-21 15:59:09 -0700

    class based components

--- communicating-with-props/src/index.js
+++ communicating-with-props/src/index.js
@@ -1,17 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import faker from 'faker';
+import CommentDetail from './components/CommentDetail'
+import ApprovalCard from './components/ApprovalCard'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+  return (
+    <div className='ui container comments'>
+      <ApprovalCard>
+        <div>
+          <h4>WARNING!!!</h4>
+          Are you sure?
+      </div>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+      </ApprovalCard>
+
+      <ApprovalCard>
+        <CommentDetail
+          author="vee"
+          timeAgo="Today at 4:20PM"
+          content="Exposed Lzzzz!!!"
+          img={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="jay"
+          timeAgo="Today at 2:13AM"
+          content="Up by 1 Lzzzzzzzz"
+          img={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="cookie"
+          timeAgo="Yesteday at 9:40PM"
+          content="Your mom must be proud"
+          img={faker.image.image()}
+        />
+      </ApprovalCard>
+    </div>
+  )
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById('root'))
+
+
+
+
