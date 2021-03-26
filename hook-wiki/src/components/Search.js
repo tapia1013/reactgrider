70183fe392b965f1ad1efd3fd6e0e9cf40ce8a01
tapia1013 <manbearpig1013@gmail.com>
2021-03-25 22:01:33 -0700

    useeffect

--- hook-wiki/src/components/Search.js
+++ hook-wiki/src/components/Search.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 
 
 const Search = () => {
+  const [term, setTerm] = useState('');
+
+
+  console.log('I RUN WITH EVERY RENDER');
+
+  // useEffect(() => {
+  //   console.log('Second argument is [] so i only run once');
+  // }, [])
+
+
+  useEffect(() => {
+    console.log('NO second argument so I run EVERY RENDER');
+  })
+
+
+  // VIDEO 154 @ 1:30S
   return (
-    <h1>Search</h1>
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            className="input"
+            type="text"
+            value={term}
+            onChange={e => setTerm(e.target.value)}
+          />
+        </div>
+      </div>
+    </div>
   )
 }
 
