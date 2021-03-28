fb2cd86762e216881bd6aa626fc1cae92cdbf67b
tapia1013 <manbearpig1013@gmail.com>
2021-03-28 15:34:07 -0700

    added route and starting header

---
+++ hook-nav/src/App.js
@@ -0,0 +1,121 @@
+import React, { useState } from 'react';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
+import Translate from './components/Translate';
+import Route from './components/Route';
+
+const items = [
+  {
+    title: 'What is React?',
+    content: 'React is a front-end Javascript framework'
+  },
+  {
+    title: 'Why use React?',
+    content: 'React is a lit JS Library foo'
+  },
+  {
+    title: 'How do you use React?',
+    content: 'You use React by creating Components'
+  }
+]
+
+
+const options = [
+  {
+    label: 'The Color Red',
+    value: 'red'
+  },
+  {
+    label: 'The Color Green',
+    value: 'green'
+  },
+  {
+    label: 'A Shade of Blue',
+    value: 'blue'
+  }
+]
+
+
+// export default () => {
+//   const [selected, setSelected] = useState(options[0]);
+//   const [showDropdown, setShowDropDown] = useState(true)
+
+//   return (
+//     <div>
+//       <button onClick={() => setShowDropDown(!showDropdown)}>Toggle Dropdown</button>
+//       {showDropdown ? (
+//         <Dropdown
+//           selected={selected}
+//           onSelectedChange={setSelected}
+//           options={options}
+//         />) : null
+//       }
+//       <h1 style={{ color: `${selected.value}` }}>{selected.label}</h1>
+//     </div>
+//   )
+// }
+
+
+
+const showAccordion = () => {
+  if (window.location.pathname === '/') {
+    return <Accordion items={items} />
+  }
+}
+
+const showList = () => {
+  if (window.location.pathname === '/list') {
+    return <Search />
+  }
+}
+
+const showDropdown = () => {
+  if (window.location.pathname === '/dropdown') {
+    return <Dropdown />
+  }
+}
+
+const showTranslate = () => {
+  if (window.location.pathname === '/translate') {
+    return <Translate />
+  }
+}
+
+
+// const showComponent = (route, component) => {
+//   return window.location.pathname === route
+//     ? component
+//     : null
+// }
+
+
+
+
+
+export default () => {
+  const [selected, setSelected] = useState(options[0])
+
+  return (
+    <div>
+      <Route path="/">
+        <Accordion items={items} />
+      </Route>
+      <Route path="/list">
+        <Search />
+      </Route>
+      <Route path="/dropdown">
+        <Dropdown
+          label="Select a color"
+          options={options}
+          selected={selected}
+          onSelectedChange={setSelected}
+        />
+      </Route>
+      <Route path="/translate">
+        <Translate />
+      </Route>
+    </div>
+  )
+}
+
