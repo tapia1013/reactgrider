a9d7066e20cf003bead1f7551fc887ae5b777759
tapia1013 <manbearpig1013@gmail.com>
2021-04-03 14:32:32 -0700

    multiple req with users

--- blog/src/reducers/postsReducer.js
+++ blog/src/reducers/postsReducer.js
@@ -1,6 +1,29 @@
 
-export default () => {
-  return 123
+export default (state = [], action) => {
+  //              BAD
+  // bad cause we reach out
+  // return document.querySelector('input')
+  // bad cause we reach out
+  // return axios.get('/posts')
+
+  //              GOOD
+  // ONLY!!! return computation from 2 arg
+  // return state + action
+
+
+  // if (action.type === 'FETCH_POSTS') {
+  //   return action.payload
+  // }
+  // return state;
+
+
+  // we usually use switch with reducers
+  switch (action.type) {
+    case 'FETCH_POSTS':
+      return action.payload;
+
+    default:
+      return state;
+  }
 }
 
-// video 261
