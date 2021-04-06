5ae271ca042d4ee17ead0cea13b5c9f193d53354
tapia1013 <manbearpig1013@gmail.com>
2021-04-06 12:53:33 -0700

    signed up and added oauth

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,14 +1,30 @@
 import React from 'react'
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 
+import StreamCreate from './streams/StreamCreate'
+import StreamEdit from './streams/StreamEdit'
+import StreamDelete from './streams/StreamDelete'
+import StreamList from './streams/StreamList'
+import StreamShow from './streams/StreamShow'
+
+import Header from './Header'
+
+
+
+// '567147132459-0nh4fjqrah399aq4oc5bplj6hasd4j1m.apps.googleusercontent.com'
 
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
         <div>
-
+          <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
