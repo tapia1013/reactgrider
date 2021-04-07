bfd74ab5f08e1c07e66cc9ba9bd00304ef69ffe5
tapia1013 <manbearpig1013@gmail.com>
2021-04-06 17:40:38 -0700

    done with authentication and redux

--- streams/client/src/components/GoogleAuth.js
+++ streams/client/src/components/GoogleAuth.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { signIn, signOut } from '../actions'
+
 
 class GoogleAuth extends Component {
-  state = { isSignedIn: null }
+  // state = { isSignedIn: null }
 
 
   componentDidMount() {
@@ -13,10 +16,13 @@ class GoogleAuth extends Component {
         scope: 'email'
       })
         .then(() => {
-          this.auth = window.gapi.auth2.getAuthInstance();
-          this.setState({
-            isSignedIn: this.auth.isSignedIn.get()
-          })
+          this.auth = window.gapi.auth2.getAuthInstance()
+
+          // this.setState({
+          //   isSignedIn: this.auth.isSignedIn.get()
+          // })
+          this.onAuthChange(this.auth.isSignedIn.get())
+
           // this.onAuthChange()
           this.auth.isSignedIn.listen(this.onAuthChange)
         })
@@ -25,10 +31,12 @@ class GoogleAuth extends Component {
 
 
   // arrow fuctions if used as callbacks so it binds
-  onAuthChange = () => {
-    this.setState({
-      isSignedIn: this.auth.isSignedIn.get()
-    })
+  onAuthChange = (isSignedIn) => {
+    if (isSignedIn) {
+      this.props.signIn(this.auth.currentUser.get().getId())
+    } else {
+      this.props.signOut()
+    }
   }
 
   onSignInClick = () => {
@@ -42,9 +50,9 @@ class GoogleAuth extends Component {
 
 
   renderAuthButton() {
-    if (this.state.isSignedIn === null) {
+    if (this.props.isSignedIn === null) {
       return null;
-    } else if (this.state.isSignedIn) {
+    } else if (this.props.isSignedIn) {
       return (
         <button onClick={this.onSignOutClick} className='ui red google button'>
           <i className='google icon' />
@@ -71,4 +79,15 @@ class GoogleAuth extends Component {
     )
   }
 }
-export default GoogleAuth;
+
+
+const mapStateToProps = (state) => {
+  return { isSignedIn: state.auth.isSignedIn }
+}
+
+
+export default connect(
+  mapStateToProps, {
+  signIn,
+  signOut
+})(GoogleAuth);
---
+++ streams/client/src/reducers/authReducer.js
@@ -0,0 +1,25 @@
+import { SIGN_IN, SIGN_OUT } from '../actions/types'
+
+const INITIAL_STATE = {
+  isSignedIn: null,
+  userId: null
+}
+
+
+// Default arg.. when our REDUX app first boots up our reducer get called one time to initialize, WE NEED a value so it WONT BE UNDEFINED
+export default (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case SIGN_IN:
+      // copy/take out old values and add new
+      return { ...state, isSignedIn: true, userId: action.payload }
+    case SIGN_OUT:
+      return { ...state, isSignedIn: false, userId: null }
+    default:
+      return state;
+  }
+}
+
+
+
+
+// VIDEO 318
