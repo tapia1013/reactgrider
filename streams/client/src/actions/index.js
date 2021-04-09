9f2e0e1bd7d741a34341b79daa105343133584b8
tapia1013 <manbearpig1013@gmail.com>
2021-04-08 20:38:59 -0700

    done for today

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,4 +1,5 @@
 import streams from '../apis/streams';
+import history from '../history';
 import {
   SIGN_IN,
   SIGN_OUT,
@@ -37,7 +38,7 @@ export const createStream = (formValues) => async (dispatch, getState) => {
 
   // force navigation/programmatic navigation
   // to get the user back to the root route
-
+  history.push('/');
 }
 
 
