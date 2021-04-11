9697716943977290a06c00061a91f66914de4d4e
tapia1013 <manbearpig1013@gmail.com>
2021-04-11 16:53:17 -0700

    added rtmp and obs DONE WITH COURSE

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 
@@ -7,8 +8,58 @@ import { fetchStream } from '../../actions';
 
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.videoRef = React.createRef()
+  }
+
   componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id)
+    const { id } = this.props.match.params
+
+    this.props.fetchStream(id);
+
+    // this is for video stream only nothing to do with fetchStream above
+    // this.player = flv.createPlayer({
+    //   type: 'flv',
+    //   url: `http://localhost:8000/live/${id}.flv`
+    // })
+    // this.player.attachMediaElement(this.videoRef.current)
+    // this.player.load()
+
+
+    this.buildPlayer()
+
+  }
+
+  // cause the didMount will be called once so we need to check if its still there, if it re-renders it wont give us an error
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+
+  // unMount is for when we go back to homepage the stream is ended, this cleans up when we move to another page, error: mediasourceEnded
+  componentWillUnmount() {
+    // tells player to stop attempting to stream video
+    this.player.destroy();
+  }
+
+
+
+  buildPlayer() {
+    const { id } = this.props.match.params
+
+    if (this.player || !this.props.stream) {
+      return;
+    }
+
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`
+    })
+    this.player.attachMediaElement(this.videoRef.current)
+    this.player.load()
+
   }
 
 
@@ -21,6 +72,11 @@ class StreamShow extends React.Component {
 
     return (
       <div>
+        <video
+          ref={this.videoRef}
+          style={{ width: '100%' }}
+          controls={true}
+        />
         <h1>{title}</h1>
         <h5>{description}</h5>
       </div>
